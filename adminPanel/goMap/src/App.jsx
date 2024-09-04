1120090d66eac3f70078db99106e92f13644c533
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2024-09-04 19:20:04 +0530

    done chnages

--- adminPanel/goMap/src/App.jsx
+++ adminPanel/goMap/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Login from './Login/Login.jsx'
 //test
